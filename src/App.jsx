1ba14de1e0c6f36009e36bdf04e1edceccb04f3a
Simon Ntini <simon.ntini@dadevs.co.za>
2022-05-24 16:27:28 +0200

    _

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {Navbar} from './component'
+
+function App() {
+  return (
+    
+    <div>
+      <Navbar />
+      {/* <button className="btn">Hello daisyUI</button> */}
+    </div>
+  )
+}
+
+export default App
