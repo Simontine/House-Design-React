760d8a5ac28aad2f9cb6441e3d0c53793d3856bd
Simon Ntini <simon.ntini@dadevs.co.za>
2022-05-26 15:20:05 +0200

    '

---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./About.css";
+
+export const About = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum eveniet
+      iusto tempore earum officia debitis asperiores aperiam voluptatibus
+      tempora doloribus, ipsam, modi dolore laboriosam quo sint. Facilis magni
+      nobis odio, libero modi similique quaerat eligendi maiores ipsum tenetur
+      reprehenderit. Nam sunt, hic nihil laudantium nemo laboriosam consequuntur
+      voluptatem voluptas provident molestias reiciendis, sint at! Sed, possimus
+      distinctio! Impedit odit eos, nobis numquam distinctio rem sint
+      exercitationem iste commodi adipisci dolorem eius facilis minus corporis
+      doloribus neque dolor in ducimus alias? Totam nulla eveniet magni ipsam
+      modi, vitae autem quod eos? Quasi magni, iste possimus nostrum pariatur
+      mollitia dignissimos sequi placeat vitae recusandae animi ratione. Et a
+      officiis commodi nemo, est ex sed! Enim cupiditate nihil ex, quod corrupti
+      esse quasi fuga id repellendus libero deserunt, dolor similique omnis,
+      quae delectus eum tempore possimus illum quisquam quidem voluptatem
+      nesciunt est asperiores harum. Ea a nihil commodi assumenda cumque
+      quisquam laudantium natus odit enim. Quam, dolores illum laborum eligendi
+      soluta cum totam nobis blanditiis, quasi quia, provident perferendis?
+      Veritatis, et dignissimos, laborum obcaecati assumenda corrupti nemo fuga
+      illum nam necessitatibus, exercitationem quasi dicta accusantium ipsam
+      voluptate veniam ducimus mollitia fugiat aspernatur deserunt illo minus!
+      Impedit architecto ullam vel ipsa quidem delectus obcaecati!
+    </div>
+  );
+};
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,56 +1,74 @@
 import React from "react";
+import "./Homepage.css";
 import { Container, Row, Col } from "react-grid-system";
+import Background from "../../images/cover-photo.jpg";
+import pages from '../pages'
+var backgroundImage = {
+  width: "10%",
+  height: "100vh",
+  backgroundImage: "url(" + { Background } + ")",
+};
 
 const Homepage = (props) => {
   return (
     <>
-      <div style={{margin:"1rem"}}>
-        <div class="carousel w-full">
-          <div id="item1" class="carousel-item w-full">
-            <img
-              src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2"
-              class="w-full"
-            />
-          </div>
-          <div id="item2" class="carousel-item w-full">
-            <img
-              src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB"
-              class="w-full"
-            />
-          </div>
-          <div id="item3" class="carousel-item w-full">
-            <img
-              src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6"
-              class="w-full"
-            />
-          </div>
-          <div id="item4" class="carousel-item w-full">
-            <img
-              src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693"
-              class="w-full"
-            />
-          </div>
-        </div>
-        <div class="flex justify-center w-full py-2 gap-2">
-          <a href="#item1" class="btn btn-xs">
-            1
-          </a>
-          <a href="#item2" class="btn btn-xs">
-            2
-          </a>
-          <a href="#item3" class="btn btn-xs">
-            3
-          </a>
-          <a href="#item4" class="btn btn-xs">
-            4
-          </a>
-        </div>
-        <Row>
-          <Col sm={4}>One of three columns</Col>
-          <Col sm={4}>One of three columns</Col>
-          <Col sm={4}>One of three columns</Col>
+      <div class="social">
+        <svg
+          role="img"
+          viewBox="0 0 24 24"
+          class="linkedin"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>LinkedIn</title>
+          <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+        </svg>
+
+        <svg
+          role="img"
+          viewBox="0 0 24 24"
+          class="facebook"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Facebook</title>
+          <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+        </svg>
+
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <title>Discord</title>
+          <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
+        </svg>
+
+        <svg
+          role="img"
+          viewBox="0 0 24 24"
+          class="twitter"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Twitter</title>
+          <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+        </svg>
+      </div>
+      <div Container style={({ margin: "3rem" }, { textAlign: "center" })}>
+        <Row className="text-color:white">
+          <Col sm={6}>
+            <a href="">One of three columns</a>{" "}
+          </Col>
+          <Col sm={6}>
+            <a href="">One of three columns</a>{" "}
+          </Col>
         </Row>
       </div>
+      {/* <div className="box"> */}
+      <Row className="bottom-nav">
+          <Col sm={6}>
+            <a href="">One of three columns</a>{" "}
+          </Col>
+          <Col sm={6}>
+            <a href="">One of three columns</a>{" "}
+          </Col>
+        </Row>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aspernatur velit!</p>
+      {/* </div> */}
     </>
   );
 };
